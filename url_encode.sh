07d58fb4a42a418a898e107cdf3be2bacf357421
ywsswy <1726208887@qq.com>
2021-01-17 23:50:46 +0800

    Update url_encode.sh

--- url_encode.sh
+++ url_encode.sh
@@ -1,19 +1,24 @@
 #!/usr/bin/env bash
 
 str="$1"
+# chinese all
+type="$2"
 
 function UrlEncode()
 {
     for ((i = 0; i < ${#1}; ++i))do
-        chr=${str:i:1}
+        chr=${1:i:1}
         ascii_code=$(printf "%d" "'$chr ")
-        if [ $ascii_code -gt 255 ];then
-            echo -n "$chr" |hexdump -C |awk -v FS="  " '{printf $2;}' |awk -v FS=" " -v OFS="%%" '{$1=$1; printf "%%";printf $0;}'
+        if [ "$2" == "all" ];then
+          echo -n "$chr" |hexdump -C |awk -v FS="  " '{printf $2;}' |awk -v FS=" " -v OFS="%%" '{$1=$1; printf "%%";printf $0;}'
         else
-            echo -n "$chr"
+          if [ $ascii_code -gt 255 ];then
+              echo -n "$chr" |hexdump -C |awk -v FS="  " '{printf $2;}' |awk -v FS=" " -v OFS="%%" '{$1=$1; printf "%%";printf $0;}'
+          else
+              echo -n "$chr"
+          fi
         fi
     done
 }
 
-echo $(UrlEncode $str)
-
+echo -n $(UrlEncode $str $type)
