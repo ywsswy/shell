cb1e851700123309948b6edaeaaa559b3340b68f
ywsswy <1726208887@qq.com>
2021-08-04 21:36:19 +0800

    feat: fix str_replace.sh for sed

--- str_replace.sh
+++ str_replace.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 if [ "${1+yes}" == "" ];then
+    # 允许$1是空字符串，但是不允许$1不传
     echo "WARN unset"
     while LFS= ;read -r line;do
         :
@@ -14,7 +15,10 @@ else
         if [ "$from_str" != ""\
             -a "$file_name" != "" ];then
             from_str="${from_str//\//\\/}"
+            from_str="${from_str//\*/\\*}"
+            from_str="${from_str//\./\\.}"
             sed -i "s/${from_str}/${to_str}/g" "$file_name"
+            # 如果from_str恰巧包含正则的字符，例如/ * .等，那么sed就会不符合预期，所以把特殊字符转义后在执行sed，确保sed是不含正则的
         fi  
     done
 fi
